0d853775bc13816a7ac9ee0fbdcaf78669f44291
Angvish Shaw <angvishvish@gmail.com>
2014-06-06 01:41:25 +0530

    adds important classes and layout markup

--- assets/js/counter-bootstrap.add.js
+++ assets/js/counter-bootstrap.add.js
@@ -248,10 +248,3 @@ var CountdownEditUI = function(qsp, uiOptions) {
 	// Trigger initial validation
 	validate();
 };
-var qsp = QueryStringHelper.parse(window.location.search);
-
-var cd = new Countdown(qsp.title, Countdown.ParseDate(qsp.when));
-var ui = new CountdownUI(cd, {title: "#title", container: "#container", when: "#whenContainer", pastClass: "past", futureClass: "future"});
-var editUI = new CountdownEditUI(qsp, {title: "#titleEdit", when: "#whenEdit" });
-
-ui.start();
